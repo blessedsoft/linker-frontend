615001299289975c7fb736f2ac7bebf81a55c83e
Olajide Salami <olajide.salami@ibedc.com>
2026-03-22 14:38:04 +0100

    feat: fix devsecops cicd

--- components/DashboardLayout.tsx
+++ components/DashboardLayout.tsx
@@ -35,7 +35,7 @@ export default function DashboardLayout({ children }: DashboardLayoutProps) {
     if (isLoading) {
         return (
             <div className="w-screen h-screen bg-[#FAFAFA] flex justify-center items-center">
-                <BeatLoader color="#633CFF" size={20} />
+                <BeatLoader color="#200c6e" size={20} />
             </div>
         );
     }
