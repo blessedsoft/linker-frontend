fda27a750a6e0f39e4a718d518927c95510c8338
Olajide Salami <olajide.salami@ibedc.com>
2026-03-22 15:07:07 +0100

    feat: test-pr

--- components/DashboardLayout.tsx
+++ components/DashboardLayout.tsx
@@ -35,7 +35,7 @@ export default function DashboardLayout({ children }: DashboardLayoutProps) {
     if (isLoading) {
         return (
             <div className="w-screen h-screen bg-[#FAFAFA] flex justify-center items-center">
-                <BeatLoader color="#200c6e" size={20} />
+                <BeatLoader color="#3b0fea" size={20} />
             </div>
         );
     }
